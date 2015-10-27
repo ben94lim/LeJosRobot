3c0df1321d3e5fb33da727664b0884e1ee99d69c
Benjamin Lim <ben94lim@gmail.com>
2015-10-27 13:49:21 +0800

    1. Added UltrasonicSensor initialisation

--- LeJos Robot/src/lab_tasks/Robot.java
+++ LeJos Robot/src/lab_tasks/Robot.java
@@ -1,8 +1,13 @@
 package lab_tasks;
 
+import java.io.IOException;
+
 import lejos.hardware.ev3.EV3;
 import lejos.hardware.ev3.LocalEV3;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.hardware.video.Video;
 import lejos.robotics.RegulatedMotor;
 
 public class Robot {
@@ -15,6 +20,19 @@ public class Robot {
 
     /** The motor on the right side of the robot */
     static RegulatedMotor rightMotor = new EV3LargeRegulatedMotor(brick.getPort("C"));
+    
+    /** Video input from the webcam */
+    static Video webcam = brick.getVideo();
+    
+    private static final int WIDTH = 160;
+    private static final int HEIGHT = 120;
+    
+    public static EV3UltrasonicSensor sonicSensor = new EV3UltrasonicSensor(SensorPort.S4);
+    
+    public Robot() throws IOException{
+    	Robot.webcam.open(WIDTH, HEIGHT);
+    }
+    
 
 }
 
