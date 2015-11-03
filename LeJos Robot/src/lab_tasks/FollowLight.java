1258696c6ca08892a1c60301168b0d72689e3fca
Benjamin Lim <ben94lim@gmail.com>
2015-11-03 18:23:25 +0800

    1. Added wall detection
    2. Improved thresholds

--- LeJos Robot/src/lab_tasks/FollowLight.java
+++ LeJos Robot/src/lab_tasks/FollowLight.java
@@ -31,17 +31,17 @@ import lejos.utility.Delay;
  */
 
 public class FollowLight {	
-	
+
 	private static final int WIDTH = 160;
 	private static final int HEIGHT = 120;
-	
+
 	// Frames and motion maps
 	private static byte [][] luminanceFrame = new byte[HEIGHT][WIDTH];
 	private static int threshold = 70;
-	
+
 	/** The EV3 brick we're controlling */
-    private EV3 brick;
-	
+	private EV3 brick;
+
 	// Motors
 	private static RegulatedMotor leftMotor = new EV3LargeRegulatedMotor(MotorPort.B);
 
@@ -49,32 +49,21 @@ public class FollowLight {
 
 	// Light features
 	private static LightFeatures aLightFeat = new LightFeatures();
-	
-	private static double oldMeanLight, newMeanLight;
-	
+
 	private static EV3ColorSensor lightSensor = new EV3ColorSensor(SensorPort.S3);
-   
+
 	private static SampleProvider getLight = lightSensor.getAmbientMode();
-   
-    private static float[] lightSample = new float[getLight.sampleSize()];
-    
 
-       
+	private static float[] lightSample = new float[getLight.sampleSize()];
 
-    
 
-	
-	/*// Randomness
-	//private static Random randGenerator = new Random();
-	private static int randDegLeft, randDegRight;
-	private static int randMotor; // 0 = left; 1 = right
-*/	 
-    public FollowLight() {
-    	
-    	// permanently store the brick in our instance variable
-    	brick = (EV3) BrickFinder.getLocal();;
-    	
-    	/*// Establish a fail-safe: pressing Escape quits
+
+	public FollowLight() {
+
+		// permanently store the brick in our instance variable
+		brick = (EV3) BrickFinder.getLocal();;
+
+		/*// Establish a fail-safe: pressing Escape quits
     	brick.getKey("Escape").addKeyListener(new KeyListener() {
     		@Override
     		public void keyPressed(Key k) {
@@ -85,9 +74,7 @@ public class FollowLight {
     			System.exit(0);
     		}
     	});*/
-		// Various initializations
-		//randDegLeft = 0;
-		//randDegRight = 0;
+
 		// Initialize luminance frame
 		for (int x=0; x<WIDTH; x += 1) {
 			for (int y=0; y<HEIGHT; y += 1) {
@@ -102,154 +89,163 @@ public class FollowLight {
 		Video video = ev3.getVideo();
 		video.open(WIDTH, HEIGHT);
 		byte[] frame = video.createFrame();
-		
-		double mot_amplif_larger = 1.0*0.6;
-		double mot_amplif_smaller = 1.0*0.3;
+
+		double mot_amplif_larger = 0.3;
+		double mot_amplif_smaller = 0.3;
 		double left_field = 0;
 		double right_field = 0;
-		
-		// Grab frame
-		video.grabFrame(frame);
-		
-		// Extract luminanceFrame
-		extractLuminanceValues(frame);
-		
-	 	// Compute light features
-		aLightFeat.compLeftRight(luminanceFrame, HEIGHT, WIDTH);
-	 	 
+
+		computeNextFrame(video, frame);
+
 		while(Button.ESCAPE.isUp()) {
-	     	
+
 			// --- Get webcam information
-			
-			// Grab frame
-			video.grabFrame(frame);
-			
-			// Extract luminanceFrame
-			extractLuminanceValues(frame);
-			
-			// Compute light features
-			aLightFeat.compLeftRight(luminanceFrame, HEIGHT, WIDTH);
-			
+
+			computeNextFrame(video, frame);
+
 			getLight.fetchSample(lightSample, 0);
-			
+
 			int i = 0;
-			while(lightSample[0]>0.4 && Button.ESCAPE.isUp())
+			while(lightSample[0]>0.3 && Button.ESCAPE.isUp())
 			{				
 				leftMotor.stop();
 				rightMotor.stop();
 				getLight.fetchSample(lightSample, 0);
-				
-				
+
+				if(checkWall(video, frame))
+					break;
+
 				System.out.printf("%d. Bright!%n", i++);
-				
-				// Grab frame
-				video.grabFrame(frame);
-				
-				// Extract luminanceFrame
-				extractLuminanceValues(frame);
-				
-			 	// Compute light features
-				aLightFeat.compLeftRight(luminanceFrame, HEIGHT, WIDTH);
-				
-				
+
+				computeNextFrame(video, frame);				
+
 				right_field = (aLightFeat.meanRight/255)*180;
-		     	left_field = (aLightFeat.meanLeft/255)*180;
-		     	
-				if ((right_field - left_field) > 30)
+				left_field = (aLightFeat.meanLeft/255)*180;
+
+				if ((right_field - left_field) > 5)
 				{
 					System.out.printf("Right");
-					rightMotor.setSpeed(50); 
-			     	leftMotor.setSpeed(50); 
-		     		leftMotor.rotate(90);
-		     		rightMotor.rotate(-45);
+					rightMotor.setSpeed(400); 
+					leftMotor.setSpeed(400); 
+					leftMotor.rotate(-100);
+					rightMotor.rotate(-250);
 				}
-		     	if ((left_field - right_field) > 30)
-		     	{
-		     		System.out.printf("Left");
-		     		rightMotor.setSpeed(50); 
-			     	leftMotor.setSpeed(50); 
-		     		rightMotor.rotate(90);
-		     		leftMotor.rotate(-45);
-		     	}
+				else if ((left_field - right_field) > 5)
+				{
+					System.out.printf("Left");
+					rightMotor.setSpeed(400); 
+					leftMotor.setSpeed(400);
+					rightMotor.rotate(-100);
+					leftMotor.rotate(-250);
+					
+				}
+				
+				else
+				{
+					System.out.printf("Back");
+					rightMotor.setSpeed(300); 
+					leftMotor.setSpeed(300);
+					rightMotor.rotate(-100);
+					leftMotor.rotate(-100);
+				}
 			}
-		     	
+
+
+			// Display
+			//System.out.println("Mean right: " + aLightFeat.meanRight);
+			//System.out.println("Mean left: " + aLightFeat.meanLeft);
+			//dispFrame();
+
+			// Regulated motors
+			//motorB.rotate((int) (mot_amplif*(aLightFeat.meanRight/255)*180), true); // true is for immediate return -> to parallelize motors
+			//motorC.rotate((int) (mot_amplif*(aLightFeat.meanLeft/255)*180));
+			// Unregulated motors
+
+			right_field = (aLightFeat.meanRight/255)*180;
+			left_field = (aLightFeat.meanLeft/255)*180;
+			System.out.printf("L: %d R:%d%n", (int)left_field, (int)right_field);
+
+			if ((right_field - left_field) > 10) {
+				System.out.printf("L:%d R:%d%n",(int)left_field, (int)right_field);
+				right_field = right_field * mot_amplif_larger;
+				left_field = left_field * mot_amplif_smaller;
+
+				//right_field = ;
+			} else if ((left_field - right_field) > 10){
+				System.out.printf("L:%d R:%d%n",(int)left_field, (int)right_field);
+				left_field = right_field * mot_amplif_larger;
+				right_field = left_field * mot_amplif_smaller;
+			}
+
+			// Go straight
+			else
+			{
+				System.out.printf("L:%d R:%d%n",(int)left_field, (int)right_field);
+				left_field = right_field * mot_amplif_smaller;
+				right_field = left_field;
+			}
+
+
+			float maxSpeed = 6;//rightMotor.getMaxSpeed();
+			rightMotor.forward();
+			leftMotor.forward();
+			System.out.printf("Spd:%d  Spd:%d%n",(int)(right_field)*(int)(maxSpeed), (int)(left_field)*(int)(maxSpeed));
+			rightMotor.setSpeed((int)(left_field)*(int)(maxSpeed)); 
+			leftMotor.setSpeed((int)(right_field)*(int)(maxSpeed)); 
 			
-	     	// Display
-	     	//System.out.println("Mean right: " + aLightFeat.meanRight);
-	     	//System.out.println("Mean left: " + aLightFeat.meanLeft);
-	     	//dispFrame();
-	     	
-	     	// Regulated motors
-	     	//motorB.rotate((int) (mot_amplif*(aLightFeat.meanRight/255)*180), true); // true is for immediate return -> to parallelize motors
-	     	//motorC.rotate((int) (mot_amplif*(aLightFeat.meanLeft/255)*180));
-	     	// Unregulated motors
-	     	
-	     	right_field = (aLightFeat.meanRight/255)*180;
-	     	left_field = (aLightFeat.meanLeft/255)*180;
-	     	
-	     	
-	     	if ((right_field - left_field) > 20) {
-	     		System.out.printf("L:%d R:%d%n",(int)left_field, (int)right_field);
-	     		right_field = right_field * mot_amplif_larger;
-	     		left_field = left_field * mot_amplif_smaller;
-	     		
-	     		//right_field = ;
-	     	} else if ((left_field - right_field) > 20){
-	     		System.out.printf("L:%d R:%d%n",(int)left_field, (int)right_field);
-	     		left_field = right_field * mot_amplif_larger;
-	     		right_field = left_field * mot_amplif_smaller;
-	     	}
-	     	
-	     	// Go straight
-	     	else
-	     	{
-	     		System.out.printf("L:%d R:%d%n",(int)left_field, (int)right_field);
-	     		left_field = right_field * mot_amplif_smaller;
-	     		right_field = left_field;
-	     	}
-	     	
-	     	
-	     	float maxSpeed = 5;//rightMotor.getMaxSpeed();
-	     	rightMotor.forward();
-	     	leftMotor.forward();
-	     	System.out.printf("L Speed:%d   R Speed:%d%n",(int)right_field, (int)left_field);
-	     	rightMotor.setSpeed((int)(left_field)*(int)(maxSpeed)); 
-	     	leftMotor.setSpeed((int)(right_field)*(int)(maxSpeed)); 
-	     	
-	     	
-	     	/**
-	     	// Compute a random move
-	     	randDegLeft = randGenerator.nextInt(181);
-	     	randDegRight = randGenerator.nextInt(181);  	
-	     	// Make move
-	     	motorB.rotate(randDegRight, true); // true is for immediate return -> to parallelize motors
-	     	motorC.rotate(randDegLeft);
-	     	// Grab frame
-	     	video.grabFrame(frame);
-	     	// Extract luminanceFrame
-	     	extractLuminanceValues(frame);
-	     	// Compute light features
-	     	aLightFeat.compLeftRight(luminanceFrame, HEIGHT, WIDTH);
-	     	newMeanLight = aLightFeat.meanTot;
-	     	// Display mean light
-	     	System.out.println("Mean light: " + newMeanLight);
-	     	// If mean light has decreased, backtrack
-	     	if (newMeanLight < oldMeanLight) {
-	     		motorB.rotate(-randDegRight, true); // true is for immediate return -> to parallelize motors
-	         	motorC.rotate(-randDegLeft);
-	     	} else {
-	     		oldMeanLight = newMeanLight;
-	     	}
-	     	**/
-	     	        	        
 		}
 		video.close();
 		lightSensor.close();
 		leftMotor.close();
 		rightMotor.close();		
 	} 
-	
- 
+
+	static boolean checkWall(Video video, byte[] frame) throws IOException
+	{
+		computeNextFrame(video, frame);
+
+		double right_field = (aLightFeat.meanRight/255)*180;
+		double left_field = (aLightFeat.meanLeft/255)*180;
+		double frame1 = right_field + left_field;
+
+		Delay.msDelay(1000);
+
+		computeNextFrame(video, frame);
+
+		right_field = (aLightFeat.meanRight/255)*180;
+		left_field = (aLightFeat.meanLeft/255)*180;
+
+		double result = right_field + left_field - frame1;
+		System.out.printf("L: %d R:%d%n", (int)left_field, (int)right_field);
+		System.out.printf("Wall %d%n", (int)Math.abs(result));
+
+		if(Math.abs(result) < 2)
+		{
+			System.out.printf("Wall %d%n", (int)result);
+			rightMotor.setSpeed(300); 
+			leftMotor.setSpeed(300);
+			rightMotor.rotate(720);
+			rightMotor.forward();
+			leftMotor.forward();
+			Delay.msDelay(1000);
+			return true;
+		}
+		
+		return false;
+	}
+
+	static void computeNextFrame(Video video, byte[] frame) throws IOException
+	{
+		// Grab frame
+		video.grabFrame(frame);
+
+		// Extract luminanceFrame
+		extractLuminanceValues(frame);
+
+		// Compute light features
+		aLightFeat.compLeftRight(luminanceFrame, HEIGHT, WIDTH);
+	}
+
 	// DO: Improve this possibly by combining with chrominance values.
 	public static void extractLuminanceValues(byte [] frame) {
 		int x,y;
@@ -261,20 +257,20 @@ public class FollowLight {
 			luminanceFrame[y][x] = frame[i];
 		}
 	}
- 
-	 public static void dispFrame() {
-		 for (int y=0; y<HEIGHT; y++) {
-			 for (int x=0; x<WIDTH; x++) {
-				 if (luminanceFrame[y][x] <= threshold) {
-					 LCD.setPixel(x, y, 1);
-				 }
-				 else {
-					 LCD.setPixel(x, y, 0);
-				 }	
-			 }
-		 }
-	 	
-	 }
+
+	public static void dispFrame() {
+		for (int y=0; y<HEIGHT; y++) {
+			for (int x=0; x<WIDTH; x++) {
+				if (luminanceFrame[y][x] <= threshold) {
+					LCD.setPixel(x, y, 1);
+				}
+				else {
+					LCD.setPixel(x, y, 0);
+				}	
+			}
+		}
+
+	}
 }
 
 
